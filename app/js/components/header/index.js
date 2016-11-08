ddb8e31972d963375f4e9091fe76b83c8c30b604
Kauê Buriti <kauepbd@gmail.com>
2016-11-08 17:19:56 -0300

    Add Header component

--- app/js/components/header/index.js
+++ app/js/components/header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Container, Column, Row} from 'components/grid/';
 import Navbar from "components/navbar/";
 import Ballon from "components/ui/ballon";
 import style from "./header.css";
@@ -8,7 +9,61 @@ const Header = () => {
     <div className={style.header}>
       <Navbar />
 
-      <Ballon>Hello guys!</Ballon>
+      <section className={style.profileContainer}>
+        <div className={style.profile}>
+          <Row>
+            <Column options={['col-xs-12', 'col-md-6']}>
+              <img className={style.profile__image} src="https://scontent.ffor2-1.fna.fbcdn.net/v/t1.0-9/12715809_1297142960312467_4707451674445037566_n.jpg?oh=4588c06d4a6ebf7d8c7a5ec2efba377e&oe=58D04D55" />
+            </Column>
+
+            <Column options={['col-xs-12', 'col-md-6']}>
+              <div className={style.profile__content}>
+                <div className={style.profile__header}>
+                  <div className={style.profile__headerTop}>
+                    <Ballon>Hello!</Ballon>
+                  </div>
+
+                  <h2 className={style.profile__name}>
+                    I'm <span className={style.nameHighlight}>Kauê Buriti</span>
+                  </h2>
+                  <p className={style.profile__bio}>
+                    In love with front-end development.
+                  </p>
+                </div>
+
+                <div className={style.profile__body}>
+                  <div className={style.profile__info}>
+                    <span className={style.profile__infoLabel}>
+                      E-mail:
+                    </span>
+                    <span className={style.profile__infoText}>
+                      kauepbd@gmail.com
+                    </span>
+                  </div>
+
+                  <div className={style.profile__info}>
+                    <span className={style.profile__infoLabel}>
+                      Phone:
+                    </span>
+                    <span className={style.profile__infoText}>
+                      +55 84 99846 7756
+                    </span>
+                  </div>
+
+                  <div className={style.profile__info}>
+                    <span className={style.profile__infoLabel}>
+                      Github:
+                    </span>
+                    <span className={style.profile__infoText}>
+                      https://github.com/kaueburiti
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </Column>
+          </Row>
+        </div>
+      </section>
     </div>
   )
 }
