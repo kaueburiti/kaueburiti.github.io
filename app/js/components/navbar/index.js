3b0cf5e72a6b715d2e1bdfe7d016b2e8a2fc5f73
Kauê Buriti <kauepbd@gmail.com>
2016-11-12 15:45:03 -0300

    Add a sticky navbar menu

--- app/js/components/navbar/index.js
+++ app/js/components/navbar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 import _ from 'lodash';
+import Sticky from "react-stickynode";
 import FontAwesome from 'react-fontawesome';
 
 import {Container, Column, Row} from 'components/grid/';
@@ -34,53 +35,55 @@ export default class Navbar extends React.Component {
     });
 
     return (
-      <nav className={style['main-menu']}>
-        <div className={overlayClasses} onClick={this.toggleMenu.bind(this)}></div>
+      <Sticky enabled={true} innerZ={100} enableTransforms={false}>
+        <nav className={style['main-menu']}>
+          <div className={overlayClasses} onClick={this.toggleMenu.bind(this)}></div>
 
-        <Container>
-          <Row options={['middle-xs']}>
-            <Column options={['col-xs-8', 'col-md-2']}>
-              <img height="50px" src="https://cdn1.iconfinder.com/data/icons/simple-icons/4096/codeschool-4096-black.png" />
-            </Column>
+          <Container>
+            <Row options={['middle-xs']}>
+              <Column options={['col-xs-8', 'col-md-2']}>
+                <img height="50px" src="https://cdn1.iconfinder.com/data/icons/simple-icons/4096/codeschool-4096-black.png" />
+              </Column>
 
-            <Column options={['col-md-10', 'last-xs', 'end-md']}>
-              <ul className={menuItemsClasses}>
-                <li className={style['main-menu__item']}>
-                  <a href="#profile">Profile</a>
-                </li>
+              <Column options={['col-md-10', 'last-xs', 'end-md']}>
+                <ul className={menuItemsClasses}>
+                  <li className={style['main-menu__item']}>
+                    <a href="#profile">Profile</a>
+                  </li>
 
-                <li className={style['main-menu__item']}>
-                  <a href="#">Skills</a>
-                </li>
+                  <li className={style['main-menu__item']}>
+                    <a href="#">Skills</a>
+                  </li>
 
-                <li className={style['main-menu__item']}>
-                  <a href="#">Experience</a>
-                </li>
-                
-                <li className={style['main-menu__item']}>
-                  <a href="#">Services</a>
-                </li>
+                  <li className={style['main-menu__item']}>
+                    <a href="#">Experience</a>
+                  </li>
 
-                <li className={style['main-menu__item']}>
-                  <a href="#">References</a>
-                </li>
+                  <li className={style['main-menu__item']}>
+                    <a href="#">Services</a>
+                  </li>
 
-                <li className={style['main-menu__item']}>
-                  <a href="#">Contact</a>
-                </li>
-              </ul>
-            </Column>
+                  <li className={style['main-menu__item']}>
+                    <a href="#">References</a>
+                  </li>
 
-            <Column options={['col-xs-4', 'last-md', 'end-xs']}>
-              <Button
-                onClick={this.toggleMenu.bind(this)}
-                className={style['main-menu__toggle']} >
-                <FontAwesome name='navicon' /> Menu
-              </Button>
-            </Column>
-          </Row>
-        </Container>
-      </nav>
+                  <li className={style['main-menu__item']}>
+                    <a href="#">Contact</a>
+                  </li>
+                </ul>
+              </Column>
+
+              <Column options={['col-xs-4', 'last-md', 'end-xs']}>
+                <Button
+                  onClick={this.toggleMenu.bind(this)}
+                  className={style['main-menu__toggle']} >
+                  <FontAwesome name='navicon' /> Menu
+                </Button>
+              </Column>
+            </Row>
+          </Container>
+        </nav>
+      </Sticky>
     );
   }
 }
