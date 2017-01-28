68b2499f6f2c056e6545f79ffb97529804911438
˜Kauê <kauepbd@gmail.com>
2017-01-28 15:04:01 -0300

    Add the basic structure to Experiences Section

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "components/header/";
 import Profile from "components/profile/";
 import Skills from "components/skills/";
+import Experiences from "components/experiences/";
 import style from "css/style.css";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Header />
       <Profile />
       <Skills />
+      <Experiences />
     </div>
   )
 }
