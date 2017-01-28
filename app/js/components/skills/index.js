f24b4388c34c96bdadfb9188b0c9dc5fde5cbd5c
˜Kauê <kauepbd@gmail.com>
2017-01-28 11:06:04 -0300

    Add the basic structure to Skills section

--- app/js/components/skills/index.js
+++ app/js/components/skills/index.js
@@ -1,108 +1,59 @@
 import React from "react";
-import {Container, Column, Row} from 'components/grid/';
+import {Section, Container, Column, Row} from 'components/grid/';
 import FontAwesome from 'react-fontawesome';
 import SubjectSection from "components/subject-section/";
 import style from "./skills.css";
 
 const Skills = () => {
   return(
-    <SubjectSection title="Skills">
-      <div id="skills" className={style.skillsContainer}>
-        <Row>
-          <Column options={['col-xs-12', 'col-sm-6']}>
-            <div className={style.skillsBox}>
-              <h3 className={style.skillsBox__title}>Professional Skills</h3>
+    <Section
+      title="Skills"
+      subheading={'Let me tell more about me, ok?'}
+      id="skills">
 
-              <div className={style.skill}>
-                <h4 className={style.skill__title}>Javascript</h4>
-                <p className={style.skill__caption}>Knowloadge about ES6, Javascript Frameworks and design patterns</p>
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star-o' />
-              </div>
+      <div className={style.skill}>
+        <Container>
+          <Row>
+            <Column options={['col-xs-12']}>
+              <ul className={style.skill__options}>
+                <li className={style.skill__option + ' ' + style['-selected']}>Javascript</li>
+                <li className={style.skill__option}>HTML/CSS</li>
+                <li className={style.skill__option}>UX/UI Design</li>
+                <li className={style.skill__option}>Project Management</li>
+                <li className={style.skill__option}>SEO</li>
+                <li className={style.skill__option}>Extra Skills</li>
+              </ul>
+            </Column>
+          </Row>
+        </Container>
 
-              <div className={style.skill}>
-                <h4 className={style.skill__title}>HTML & CSS</h4>
-                <p className={style.skill__caption}>Knowloadge about ES6, Javascript Frameworks and design patterns</p>
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-              </div>
+        <header className={style.skill__header}>
+          <Container>
+            <Row>
+              <Column options={['col-xs-12']}>
+                <div className={style.skill__headerContentContainer}>
+                  <div className={style.skill__headerContent}>
+                    <h1 className={style.skill__headerTitle}>Javascript</h1>
+                    <p className={style.skill__headerSubheading}>
+                      Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                    </p>
+                  </div>
+                </div>
+              </Column>
+            </Row>
+          </Container>
+        </header>
 
-              <div className={style.skill}>
-                <h4 className={style.skill__title}>Design & UI/UX</h4>
-                <p className={style.skill__caption}>Knowloadge about ES6, Javascript Frameworks and design patterns</p>
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star-o' />
-              </div>
-
-              <div className={style.skill}>
-                <h4 className={style.skill__title}>Project Management</h4>
-                <p className={style.skill__caption}>Knowloadge about ES6, Javascript Frameworks and design patterns</p>
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star-o' />
-              </div>
-            </div>
-          </Column>
-
-          <Column  options={['col-xs-12', 'col-sm-6']}>
-            <div className={style.skillsBox}>
-              <h3 className={style.skillsBox__title}>Personal Skills</h3>
-
-              <div className={style.skill}>
-                <h4 className={style.skill__title}>English fluence</h4>
-                <p className={style.skill__caption}>Knowloadge about ES6, Javascript Frameworks and design patterns</p>
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star-half-empty' />
-                <FontAwesome className={style.skill__star} name='star-o' />
-              </div>
-
-              <div className={style.skill}>
-                <h4 className={style.skill__title}>Punctual</h4>
-                <p className={style.skill__caption}>Knowloadge about ES6, Javascript Frameworks and design patterns</p>
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-              </div>
-
-              <div className={style.skill}>
-                <h4 className={style.skill__title}>Leadership</h4>
-                <p className={style.skill__caption}>Knowloadge about ES6, Javascript Frameworks and design patterns</p>
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star-half-empty' />
-                <FontAwesome className={style.skill__star} name='star-o' />
-              </div>
-
-              <div className={style.skill}>
-                <h4 className={style.skill__title}>Communicative</h4>
-                <p className={style.skill__caption}>Knowloadge about ES6, Javascript Frameworks and design patterns</p>
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star' />
-                <FontAwesome className={style.skill__star} name='star-o' />
-              </div>
-            </div>
-          </Column>
-        </Row>
+        <div className={style.skill__content}>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+          explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+          odit aut fugit, sed quia consequuntur magni dolores eos qui
+          consequuntur ratione voluptatem sequi nesciunt
+        </div>
       </div>
-    </SubjectSection>
+    </Section>
   )
 }
 
