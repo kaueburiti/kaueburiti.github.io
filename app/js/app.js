f00879ef25594eafa6cee9baf5b0483f4e57c32d
˜Kauê <kauepbd@gmail.com>
2017-02-03 20:25:03 -0300

    Add Footer structure

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@ import Profile from "components/profile/";
 import Skills from "components/skills/";
 import Experiences from "components/experiences/";
 import References from "components/references/";
+import Footer from "components/footer/";
 import style from "css/style.css";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Skills />
       <Experiences />
       <References />
+      <Footer />
     </div>
   )
 }
