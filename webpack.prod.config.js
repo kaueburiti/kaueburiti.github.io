8a284641b74ab5f0e0db897ab58ed770b4fb7703
Kauê Buriti <kauepbd@gmail.com>
2017-02-18 20:51:00 -0300

    Add loader to images that compress and minify images size

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -42,8 +42,11 @@ module.exports = {
         loader: 'file-loader'
       },
       {
-        test: /\.(png|jpg|gif)$/,
-        loader: 'file-loader'
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
       }
     ]
   },
